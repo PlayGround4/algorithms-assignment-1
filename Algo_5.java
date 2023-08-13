62f4086f80dd7c5b00af98dc5250212e08de4671
Hasara Thilakarathna <hasarathilakarathnaa4@gmail.com>
2023-08-13 20:06:24 +0530

    Algorithm 5 modified

--- Algo_5.java
+++ Algo_5.java
@@ -6,14 +6,21 @@ public class Algo_5 {
 
   public static void main(String[] args) {
     String text = "";
-
+loop:
     do {
       System.out.print("Enter a text: ");
       text = scanner.nextLine();
 
       char[] strArray = text.toCharArray();
+      for (int i = 0; i < strArray.length; i++) {
+          if (strArray[i] >=128 || strArray[i]<0) {
+            System.out.println("Invalid input!");
+            continue loop;
+        }
+        }
 
       if (text.isBlank()) System.out.println("Invalid Input!"); 
+      
 
       else {
         for (int i = 0, j = strArray.length - 1;i < strArray.length / 2;i++, j--) {
@@ -24,13 +31,13 @@ public class Algo_5 {
         System.out.print("Reversed text: ");
     
         for (int i = 0; i < strArray.length; i++) {
-          if (strArray[i] <128 || strArray[i]>0) System.out.print(strArray[i]);
+          if (strArray[i] <128 && strArray[i]>0) System.out.print(strArray[i]);
         }
        
       }
 
       System.out.println();
 
-    } while (text.isBlank());
+    } while (true);
   }
 }
