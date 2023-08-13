6d1ee0ffdf211b0454522344fa3e572219ac2ba8
Hasara Thilakarathna <hasarathilakarathnaa4@gmail.com>
2023-08-13 15:28:34 +0530

    Complete algorithm - 6

---
+++ Algo_6.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Algo_6{
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        String inputStr = "";
+       
+
+        do{
+            System.out.print("Enter a sentence: ");
+            inputStr = scanner.nextLine();
+
+            if(inputStr.isBlank()) System.out.println("Invalid Input!");
+
+            else{
+                
+                String[] strArray = inputStr.split(" ");
+                
+
+                for (int i = 0, j=strArray.length-1; i < strArray.length/2; i++,j--) {
+                    String temp = strArray[i];
+                    strArray[i] = strArray[j];
+                    strArray[j] = temp;
+                }
+
+                System.out.print("Reversed sentence: ");
+                for (int i = 0; i < strArray.length; i++) {
+                    System.out.print(strArray[i]+" ");
+                }
+            }
+
+
+        }while(inputStr.isBlank());
+        
+    }
+}
